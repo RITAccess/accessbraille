546c64ec444f641008e7a612e414598e9e2dcf0f
Piper Chester <piperchester@gmail.com>
2013-03-14 10:27:43 -0400

    Prepared for implementation of backspace sound

--- AccessBraille/AccessBraille/BrailleTyperController.m
+++ AccessBraille/AccessBraille/BrailleTyperController.m
@@ -61,6 +61,7 @@
     // Audio
     SystemSoundID enabledSound;
     SystemSoundID disabledSound;
+    SystemSoundID backspaceSound;
 
 }
 
@@ -73,12 +74,13 @@
 
 # pragma mark - ViewController Methods
 
+/**
+ Runs after load
+ */
 - (void)viewDidLoad {
-    /**
-        Runs after load
-     */
-//    NSLog(@"BTC Loaded");
+
     [super viewDidLoad];
+    
     // Braille Recognizer Gestures
     BROneTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(BRTap:)];
         [BROneTap setNumberOfTouchesRequired:1];
@@ -130,7 +132,6 @@
     bi = [[BrailleInterpreter alloc] initWithViewController:self];
     
     // Draw views
-    
     enabled = [[Enabled alloc] initWithFrame:CGRectMake(971.5, 695.5, 44, 44)];
     enabled.enable = FALSE;
     [self.view addSubview:enabled];
@@ -138,6 +139,7 @@
     // Audio
     enabledSound = [self createSoundID:@"hop.mp3"];
     disabledSound = [self createSoundID:@"disable.mp3"];
+    backspaceSound = [self createSoundID:@"hop.mp3"];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
@@ -248,6 +250,7 @@
             
         } else if ([bi getAverageYValue] - 100 < [reg locationInView:reg.view].y && [reg locationInView:reg.view].x >= backSpaceBuf) {
             [_TextDrawing removeCharacter]; // Calls TextOut's removeCharacter()
+            AudioServicesPlaySystemSound(backspaceSound);
         }
     }
 }
