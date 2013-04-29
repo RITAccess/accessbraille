3962b5041ba649713ed923bdd58dc6f08fd45daa
Piper Chester <piperchester@gmail.com>
2013-04-29 10:05:37 -0400

    Edited instructions text

--- AccessBraille/AccessBraille/FlashCard.h
+++ AccessBraille/AccessBraille/FlashCard.h
@@ -11,7 +11,7 @@
 
 NSString *const instructionsText = @"In Flash Card mode, users will type out words using the ABKeyboard to complete the word.The word will be spoken to the user letter by letter.";
 NSString *const settingsText = @"The user is able to adjust the settings later within the application.";
-NSString *const welcomeText = @"Welcome to Flash Card mode! Tap to the left of the screen to adjust settings, in the center to read the instructions, or to the right of the screen to start playing.";
+NSString *const welcomeText = @"Welcome to Flash Card mode! Tap once to hear the instructions, tap twice to hear settings, or swipe to the right of the screen to start playing.";
 
 NSInteger const maxCards = 2930;
 
