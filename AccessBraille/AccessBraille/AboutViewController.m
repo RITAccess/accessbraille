f2b797a393fdac677727a30c7e216dea2a6a5716
Piper Chester <piperchester@gmail.com>
2013-06-10 11:40:48 -0400

    Adding About menu. Updated .plists, added icons, and added to SB.

---
+++ AccessBraille/AccessBraille/AboutViewController.m
@@ -0,0 +1,38 @@
+//
+//  AboutViewController.m
+//  AccessBraille
+//
+//  Created by Piper Chester on 6/10/13.
+//  Copyright (c) 2013 RIT. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
