733654462eab758273309d0b04c784f83b13a9c7
Piper Chester <piperchester@gmail.com>
2013-06-10 14:15:28 -0400

    Changing images.

--- AccessBraille/AccessBraille/AboutViewController.m
+++ AccessBraille/AccessBraille/AboutViewController.m
@@ -18,7 +18,6 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
     }
     return self;
 }
@@ -26,13 +25,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
