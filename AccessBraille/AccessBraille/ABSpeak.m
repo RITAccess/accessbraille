e6acc58d32ffef4e60c3a56e8b399dded6f64bef
Michael Timbrook <mpt2360@rit.edu>
2013-06-24 16:36:52 -0400

    Fixed multi version suport

--- AccessBraille/AccessBraille/ABSpeak.m
+++ AccessBraille/AccessBraille/ABSpeak.m
@@ -6,35 +6,36 @@
 //  Copyright (c) 2013 RIT. All rights reserved.
 //
 
+#import <Availability.h>
 #import "ABSpeak.h"
 
-#if __IPHONE_7_0
-
-#import <AVFoundation/AVSpeechSynthesis.h>
-
+#if defined(__IPHONE_7_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_7_0
+    #import <AVFoundation/AVSpeechSynthesis.h>
 #endif
 
-
 @implementation ABSpeak
-
+#if defined(__IPHONE_7_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_7_0
 @synthesize fliteController;
 @synthesize slt;
+#endif
 
 - (void)speakString:(NSString *)string {
     
-#if __IPHONE_7_0
+    if(NSClassFromString(@"AVSpeechSynthesizer")) {
+        
+        AVSpeechSynthesizer *speak = [[AVSpeechSynthesizer alloc] init];
+        AVSpeechUtterance *talk = [AVSpeechUtterance speechUtteranceWithString:string];
+        [speak speakUtterance:talk];
+        
+    } else {
+
+        [self.fliteController say:string withVoice:self.slt];
     
-    AVSpeechSynthesizer *speak = [[AVSpeechSynthesizer alloc] init];
-    AVSpeechUtterance *talk = [AVSpeechUtterance speechUtteranceWithString:string];
-    [speak speakUtterance:talk];
-    
-#else
-    
-    [self.fliteController say:string withVoice:self.slt];
-    
-#endif
+    }
     
 }
+
+#if defined(__IPHONE_7_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_7_0
 
 - (FliteController *)fliteController {
 	if (fliteController == nil) {
@@ -49,5 +50,7 @@
 	}
 	return slt;
 }
+
+#endif
 
 @end
