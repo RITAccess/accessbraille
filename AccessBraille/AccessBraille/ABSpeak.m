13311bbc85b673366708e86481ed22da4e46e904
Piper Chester <piperchester@gmail.com>
2013-07-02 10:56:15 -0400

    Integrating updated ABSpeak from framework.

--- AccessBraille/AccessBraille/ABSpeak.m
+++ AccessBraille/AccessBraille/ABSpeak.m
@@ -6,16 +6,36 @@
 //  Copyright (c) 2013 RIT. All rights reserved.
 //
 
+#import <Availability.h>
 #import "ABSpeak.h"
 
-@implementation ABSpeak
+#if defined(__IPHONE_7_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_7_0
+    #import <AVFoundation/AVSpeechSynthesis.h>
+#endif
 
+@implementation ABSpeak
+#if defined(__IPHONE_7_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_7_0
 @synthesize fliteController;
 @synthesize slt;
+#endif
 
 - (void)speakString:(NSString *)string {
-    [self.fliteController say:string withVoice:self.slt];
+    
+    if(NSClassFromString(@"AVSpeechSynthesizer")) {
+        
+        AVSpeechSynthesizer *speak = [[AVSpeechSynthesizer alloc] init];
+        AVSpeechUtterance *talk = [AVSpeechUtterance speechUtteranceWithString:string];
+        [speak speakUtterance:talk];
+        
+    } else {
+
+        [self.fliteController say:string withVoice:self.slt];
+    
+    }
+    
 }
+
+#if defined(__IPHONE_7_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_7_0
 
 - (FliteController *)fliteController {
 	if (fliteController == nil) {
@@ -30,5 +50,7 @@
 	}
 	return slt;
 }
+
+#endif
 
 @end
