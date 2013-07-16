aab10a4a8d7bf1761fc5297dba8a0aac9bf8c880
Piper Chester <piperchester@gmail.com>
2013-07-16 09:08:45 -0400

    Removing old Settings classes. Implementing new settings UI.

---
+++ AccessBraille/AccessBraille/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  AccessBraille
+//
+//  Created by Piper Chester on 7/16/13.
+//  Copyright (c) 2013 RIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ AccessBraille/AccessBraille/SettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.m
+//  AccessBraille
+//
+//  Created by Piper Chester on 7/16/13.
+//  Copyright (c) 2013 RIT. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self.view setFrame:CGRectMake(0, 0, 1024, 768)];
+    [self.view setNeedsDisplay];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+}
+
+@end
