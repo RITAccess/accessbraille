9427b7f93a9ceb89a06020cefa1733b1de401b08
Piper Chester <piperchester@gmail.com>
2013-08-08 11:56:29 -0400

    Initial redesign of instructions. Can resolve #191.

---
+++ AccessBraille/AccessBraille/InstructionsViewController.h
@@ -0,0 +1,15 @@
+//
+//  InstructionsViewController.h
+//  AccessBraille
+//
+//  Created by Piper Chester on 8/8/13.
+//  Copyright (c) 2013 RIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstructionsViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIView *labelContainerView;
+
+@end
---
+++ AccessBraille/AccessBraille/InstructionsViewController.m
@@ -0,0 +1,29 @@
+//
+//  InstructionsViewController.m
+//  AccessBraille
+//
+//  Created by Piper Chester on 8/8/13.
+//  Copyright (c) 2013 RIT. All rights reserved.
+//
+
+#import "InstructionsViewController.h"
+
+@interface InstructionsViewController ()
+
+@end
+
+@implementation InstructionsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [_labelContainerView.layer setCornerRadius:20];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
